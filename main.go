4bd116aa2db0597d9d944fb82e81d12db472eeb5
Jeffrey Koehler <koehlerjj@msoe.edu>
2018-03-05 11:41:02 -0600

    inital commit adding original files

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"sync"
+	"fmt"
+
+	//"os"
+	
+	"State_processor/netcode"
+	"strings"
+)
+
+const(
+	differentInfoDelimiter string = "¡"
+	nameTextDelimiter string = ":"
+)
+
+
+func main() {
+
+	controller()
+	//go processor("1.1.1.1:80")
+	//for {}
+	
+}
+
+func controller(){
+	var l sync.Mutex
+	var sum = 0
+	var num = 1000000
+	var threads = 0
+
+	for i := 0; i < num; i++{
+		l.Lock()
+		threads = threads+1
+		l.Unlock()
+		go add(&l,&sum,num, &threads)
+
+	}
+	
+	for threads > 0{
+	}
+	fmt.Printf("total:%d", sum)
+
+}
+
+func add(l *sync.Mutex, sum *int, num int, threads *int){
+	var s = 0
+	for i := 0; i < num; i++{
+		s++
+	}
+	l.Lock()
+		*sum = *sum + s
+
+	*threads = *threads-1
+	l.Unlock()
+
+}
+
+func processor(controllerAddress string){
+	//create and send to the controller the receiver for the 
+	inputChannel := make(chan string, 1024) 
+	commandAddress := netcode.ArbitraryHost(inputChannel)
+	commandAddressString := "newProcessor" + nameTextDelimiter + commandAddress
+	netcode.SendMessage(commandAddressString, controllerAddress)
+	var commandString string
+	var commands []string
+	for{
+		commandString = <- inputChannel
+		commands = textSplitter(commandString, differentInfoDelimiter, nameTextDelimiter)
+
+		switch strings.ToLower(commands[0]) {
+		case "newjob"   :
+		case "runjob"   :
+		case "removejob":
+		case "listjobs" :
+		case "ping"     :
+			netcode.SendMessage("pong", commands[1]) 
+		default:
+		
+		
+		}
+	}
+}
+
+func textSplitter(text, primaryDelim, secondairyDelim string) []string{
+
+	pairs := strings.Split(text, primaryDelim)
+	var split []string
+	for _, v := range pairs {
+		fmt.Printf("%s\n", v)
+		split = append(split, strings.SplitN(v, secondairyDelim , 2)...)
+	}
+	return split
+}
+
+
---
+++ netcode/netcode.go
@@ -0,0 +1,120 @@
+package netcode
+
+import(
+	"net"
+	"log"
+	"os"
+	"fmt"
+	"strings"
+	"time"
+	"strconv"
+
+)
+
+const(
+	bufferSize int = 1024
+)
+
+//TestNetCode sends and prints 1000 messages to an arbitrarily created server
+func TestNetCode(){
+	done1 := make(chan string, 1024)
+	address := ArbitraryHost(done1)
+	fmt.Printf("inbound IP: %s \n", address)
+	for i := 0; i < 1000; i++{
+		go SendMessage("This is message " + strconv.Itoa(i), address)
+		fmt.Printf("Received :%s\n", <-done1)
+		time.Sleep(time.Millisecond * 100)	
+	}
+}
+
+//SendMessage takes in a message and address string and sends that message to that address. 
+//this code is designed to act as part of a simple 2 piece send and receive 
+func SendMessage(message string, address string) {
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		//handle error
+		panic("oops")
+	}
+	conn.Write([]byte(message))
+	conn.Close()
+}
+
+//PollMessage takes in a message and address string and sends that message to that address. 
+//this code is designed to act as part of a simple 2 piece send and receive 
+func PollMessage(message string, address string) (response string) {
+	conn, err := net.Dial("tcp", address)
+	defer conn.Close()
+	buf := make([]byte, bufferSize)
+	if err != nil {
+		// handle error
+		panic("oops")
+	}
+	conn.Write([]byte(message))
+	//fmt.Fprintf(conn, "stuff GET / HTTP/1.0\r\n\r\n")
+	_, err = conn.Read(buf)
+	return strings.Trim(string(buf), string(0))
+}
+
+//ArbitraryHost hosts an arbitrarily located tcp server. All data received is sent to the provided channel.
+//The returned string is the string that the host is listening to in the form of xxx.xxx.xxx.xxx:port
+func ArbitraryHost(output chan string) (address string){
+	l, err := net.Listen("tcp", getOutboundIP().String() +":0")
+
+	if err != nil {
+		fmt.Println("Error initialing listener:", err.Error())
+		os.Exit(1)
+	}
+	address = l.Addr().String()
+	go arbitraryHostListen(output, l)
+	return address
+}
+
+func arbitraryHostListen(output chan string, l net.Listener ){
+	defer l.Close()
+
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		
+		if err != nil {
+			fmt.Println("Error  d accepting: ", err.Error())
+			os.Exit(1)
+		}
+		// Handle connections in a new goroutine.
+		go handleInput(conn, output)
+	}
+
+}
+
+func handleInput (conn net.Conn, output chan string) {
+	// Make a buffer to hold incoming data.
+	buf := make([]byte, bufferSize)
+	// Read the incoming connection into the buffer.
+	_, err := conn.Read(buf)
+	
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+
+	output <- strings.Trim(string(buf), string(0))
+	
+	// Send a response back to incoming connection.
+	conn.Write([]byte("pong"))
+	// Close the connection when you're done with it.
+	conn.Close()
+}
+
+//getOutboundIP retrieves the IP of the current system as connected to the internet
+//by pinging the 8.8.8.8:80 address and examining the connection created. 
+//https://stackoverflow.com/a/37382208/3073088
+func getOutboundIP() net.IP {
+    conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+    return localAddr.IP
+}
